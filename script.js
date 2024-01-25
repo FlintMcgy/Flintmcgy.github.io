eae9fa69072425e6c0ff5b68bc5e98c43405ebe5
Flint <148150677+FlintMcgy@users.noreply.github.com>
2024-01-25 05:36:19 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -6,11 +6,9 @@ window.onscroll = function () {
   const animatedText = document.querySelector(".animated-text");
 
   if (prevScrollpos > currentScrollPos) {
-    // Scrolling up, show the animated text with fadeInUp animation
     animatedText.style.opacity = "1";
     animatedText.style.animation = "fadeInUp 0.5s ease-out";
   } else {
-    // Scrolling down, hide the animated text with fadeOutDown animation
     animatedText.style.opacity = "0";
     animatedText.style.animation = "fadeOutDown 0.5s ease-out";
   }
